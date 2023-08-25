406fc5bb826d4336787596a91f7949e521387ef1
Pier-Angelo Gaetani <pgaetani@worth.systems>
2023-08-25 17:23:33 +0200

    Application insights instrumentation

--- application.py
+++ application.py
@@ -1,4 +1,7 @@
 from app.performance import init_performance_monitoring
+from opencensus.ext.azure.trace_exporter import AzureExporter
+from opencensus.ext.flask.flask_middleware import FlaskMiddleware
+from opencensus.trace.samplers import ProbabilitySampler
 
 init_performance_monitoring()
 
@@ -6,5 +9,11 @@ from flask import Flask  # noqa
 from app import create_app  # noqa
 
 application = Flask("app")
+
+FlaskMiddleware(
+    application,
+    exporter=AzureExporter(),
+    sampler=ProbabilitySampler(rate=1.0),
+)
 
 create_app(application)
