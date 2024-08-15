eab64b49ff4e82476ea8b572f0e46ac6d14689a1
UltimatePancake <pier.gaetani@gmail.com>
2024-08-15 11:34:21 +0200

    WHS-004 rate limiting implementation

--- app/limiters.py
+++ app/limiters.py
@@ -1,19 +1,18 @@
 from enum import Enum
+from flask import current_app
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_login import current_user
 
-from app.config import configs
-
 
 limiter = Limiter(
     get_remote_address,
-    storage_uri=configs.get("REDIS_URL", "memory://"),
-    default_limits=[configs.get("DEFAULT_RATE_LIMIT", "1/minute")],
+    storage_uri=current_app.config("REDIS_URL"),
+    default_limits=[current_app.config("DEFAULT_RATE_LIMIT")],
 )
 
 
 class RateLimit(Enum):
     NO_LIMIT = limiter.exempt
-    INTERNAL_LIMIT = limiter.limit(configs.get("INTERNAL_RATE_LIMIT", "12/minute"))
-    USER_LIMIT = limiter.limit(configs.get("USER_RATE_LIMIT", "1/second"), key_func=lambda: current_user.id)
+    INTERNAL_LIMIT = limiter.limit(current_app.config("INTERNAL_RATE_LIMIT"))
+    USER_LIMIT = limiter.limit(current_app.config("USER_RATE_LIMIT"), key_func=lambda: current_user.id)
