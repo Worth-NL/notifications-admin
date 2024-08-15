5a9080670051bd940ea77f9d35b087e6fe091f9e
UltimatePancake <pier.gaetani@gmail.com>
2024-08-15 11:55:27 +0200

    WHS-004 rate limiting implementation

--- app/limiters.py
+++ app/limiters.py
@@ -7,12 +7,12 @@ from flask_login import current_user
 
 limiter = Limiter(
     get_remote_address,
-    storage_uri=current_app.config("REDIS_URL"),
-    default_limits=[current_app.config("DEFAULT_RATE_LIMIT")],
+    storage_uri=current_app.config["REDIS_URL"],
+    default_limits=[current_app.config["DEFAULT_RATE_LIMIT"]],
 )
 
 
 class RateLimit(Enum):
     NO_LIMIT = limiter.exempt
-    INTERNAL_LIMIT = limiter.limit(current_app.config("INTERNAL_RATE_LIMIT"))
-    USER_LIMIT = limiter.limit(current_app.config("USER_RATE_LIMIT"), key_func=lambda: current_user.id)
+    INTERNAL_LIMIT = limiter.limit(current_app.config["INTERNAL_RATE_LIMIT"])
+    USER_LIMIT = limiter.limit(current_app.config["USER_RATE_LIMIT"], key_func=lambda: current_user.id)
