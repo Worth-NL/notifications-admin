2039a2456281efde644d7da8ac9ee08b40915499
José Rodrigo Abril Lara <rabril@worth.systems>
2025-02-03 13:12:55 +0100

    merged alphagov main merge progress

--- app/version.py
+++ app/version.py
@@ -1,2 +1,2 @@
-__git_commit__ = "35be5238defd529187895f11853d79c1d454195e"
-__time__ = "2024-11-21T12:03:21"
+__git_commit__ = "1c43c4bcbfd1c5a8131a65ab1e35a766ef94ddac"
+__time__ = "2025-02-03T12:43:26"
