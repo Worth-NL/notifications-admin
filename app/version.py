4c2eea2708c3381ae08009bcb690b7c542feb2bf
José Rodrigo Abril Lara <rabril@worth.systems>
2025-02-27 15:42:49 +0100

    added the correct LandRegistry-frontend-jinja pkg

--- app/version.py
+++ app/version.py
@@ -1,2 +1,2 @@
-__git_commit__ = "e11b3dcf2b31bc25df2176dd94614f06bb3638eb"
-__time__ = "2025-02-27T10:49:19"
+__git_commit__ = "1ccecacd17c526a777a925016f182292a1b87456"
+__time__ = "2025-02-27T15:04:58"
